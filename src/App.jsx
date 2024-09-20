729f9a3a624e8134dfdaf9cc28f4904c34e8a447
anayib <nayib.abdala@gmail.com>
2024-09-20 16:28:32 +0200

    Counter console.log simplest version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 export default function ToDoList() {
+  
+  let [counter, setCounter] =useState(0);
+
+  let handleChange = (e)  => {
+    e.preventDefault()
+    console.log(e.target.name);
+  }
+  
   return (
     <>
-    <h1>Lista de Tareas</h1>
-      <ul>
-        <li>Hacer compras</li>
-        <li>Llamar cliente</li>
-        <li>Revisar calendario</li>
-      </ul>
+    <h1>Counter</h1>
+    <p>{counter}</p>
+
+    <button onClick={handleChange} name="incrase">Increment</button>
+    <button onClick={handleChange} name="decrease">Decrease</button>
     </>
   );
 }
