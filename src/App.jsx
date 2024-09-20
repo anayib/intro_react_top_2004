b2e04973f9a329f307c1709715e40a646e7ddd2c
anayib <nayib.abdala@gmail.com>
2024-09-20 16:15:55 +0200

    Add static todolist

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function ToDoList() {
+  return (
+    <>
+    <h1>Lista de Tareas</h1>
+      <ul>
+        <li>Hacer compras</li>
+        <li>Llamar cliente</li>
+        <li>Revisar calendario</li>
+      </ul>
+    </>
+  );
+}
