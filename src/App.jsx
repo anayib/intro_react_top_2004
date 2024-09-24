776f3212b0eabb5512535d94165cc63cbcbec8fb
anayib <nayib.abdala@gmail.com>
2024-09-24 14:52:48 +0200

    Basic Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,20 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
 
-export default function Post() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const postsURL = 'https://jsonplaceholder.typicode.com/posts';
-
-  useEffect(() => {
-    try {
-      fetch(postsURL)
-      .then((resp) => {
-        if (resp.status !== 200) {
-          throw new Error(error.message);
-        }
-        return resp.json();
-      })
-      .then((posts) => { 
-        setPosts(posts)
-        setLoading(false)
-        
-    })      
-    } catch (error) {
-      setError(error);
-      setLoading(false)
-    }
-  
-  }, []);
-
-  if (loading) return <div>Data is loading ...</div>;
-
-  if (error) return <div>Error: {error.message}</div> ;
-
-  return(
-    <>
-      <ul>{posts.map((post) => {
-        return <li key={post.id}>{post.title}</li>
-      })}
-      </ul>
-    </>
-  )
+function Home() {
+  return <h1>Home Page</h1>
 };
+
+function About() {
+  return <h1>About</h1>
+};
+
+export default function App(){
+  return(
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+  </Router>
+  )
+}
